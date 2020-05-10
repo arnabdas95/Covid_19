326d784db42c62070b00e413545cd198f6e61685
arnabdas95 <dasarnab1411995@gmail.com>
2020-05-10 08:20:36 +0530

    Fix None error

--- Covid19/CoronaUpdater/views.py
+++ Covid19/CoronaUpdater/views.py
@@ -1,28 +1,36 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 import requests
-import json
-
-from django.urls import reverse
 
 url = 'https://api.covid19india.org/state_district_wise.json'
 page = requests.get(url)
 data = page.json()
-state ='West Bengal'
-area ='Kolkata'
-def home(request):
-        global data,state,area
-        if request.method == 'POST'  and 'statename' in request.POST:
-                state = request.POST.get("st")
-        return render(request, 'home.html', {'data': data})
+state = area = ''
+confirmed = active = deceased = recovered = ''
+val = 0
 
+
+def home(request):
+
+    return render(request, 'home.html', {'data': data})
 
 
 def details(request):
-        global state
-        if request.method == 'POST' and 'statename' in request.POST:
-                state = request.POST.get("st")
+    global state, confirmed, area, val, active, recovered, deceased
 
+    if request.method == 'POST' and 'state_name' in request.POST:
+        state = request.POST.get("st")
+    district = data[state]['districtData']
+
+    if request.method and 'area_name' in request.POST:
         area = request.POST.get("area")
-        district = data[state]['districtData']
+        confirmed = district[area]['confirmed']
+        active = district[area]['active']
+        deceased = district[area]['deceased']
+        recovered = district[area]['recovered']
+        val = 1
+    else:
+        val = 0
 
-        return render(request,'details.html',{'state':state,'area':area,'district': district})
+    return render(request, 'details.html',
+                  {'state': state, 'area': area, 'district': district, 'confirmed': confirmed, 'active': active,
+                   'deceased': deceased, 'recovered': recovered, 'val': val})
