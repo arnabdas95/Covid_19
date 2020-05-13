a43fb23a2c84edb6e076e279ed79e216aa03500e
arnabdas95 <dasarnab1411995@gmail.com>
2020-05-13 10:13:16 +0530

    commit

--- Covid19/Covid19/settings.py
+++ Covid19/Covid19/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^3^(*)548efg^uwupyk(aaq8nkw7gq0cava#8pg4ywb2gsp*vh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['covid-19-updater.herokuapp.com','covid-19-updater.com','herokuapp.com','127.0.0.1']
 
